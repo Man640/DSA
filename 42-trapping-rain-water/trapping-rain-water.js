a8d6aded874d81582bb373aff91575bced6de57b
MANU AP <manu.ap.dev@gmail.com>
2026-07-14 23:10:42 +0530

    Time: 1 ms (66.21%) | Memory: 55.1 MB (70.13%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    if (!height || height.length === 0) return 0;
+
+    let left = 0, right = height.length - 1;
+    let leftMax = 0, rightMax = 0;
+    let totalWater = 0;
+
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                totalWater += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                totalWater += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return totalWater;
+
+};
