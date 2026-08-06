011186fc25da4119a8f16626634c9d54c92e2e77
MANU AP <manu.ap.dev@gmail.com>
2026-08-06 17:04:08 +0530

    Time: 77 ms (5.17%) | Memory: 77.4 MB (5.17%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.ts
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.ts
@@ -1,23 +1,20 @@
 function firstUniqChar(s: string): number {
     const freq = new Map<string, number>();
 
-    // Count frequency of each character
     for (const char of s) {
         freq.set(char, (freq.get(char) || 0) + 1);
     }
 
-    // Queue storing [character, index]
     const queue: [string, number][] = [];
+    let front = 0;
 
-    // Process characters in order
     for (let i = 0; i < s.length; i++) {
         queue.push([s[i], i]);
 
-        // Remove characters that are no longer unique
-        while (queue.length > 0 && freq.get(queue[0][0])! > 1) {
-            queue.shift();
+        while (front < queue.length && freq.get(queue[front][0])! > 1) {
+            front++;
         }
     }
 
-    return queue.length > 0 ? queue[0][1] : -1;
+    return front < queue.length ? queue[front][1] : -1;
 }
