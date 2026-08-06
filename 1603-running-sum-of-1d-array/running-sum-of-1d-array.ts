514b6ad7d9f9adeb97b222ff089cd7a9e8baf4f8
MANU AP <manu.ap.dev@gmail.com>
2026-08-06 17:08:10 +0530

    Time: 1 ms (39.43%) | Memory: 55.9 MB (81.07%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.ts
@@ -0,0 +1,11 @@
+function runningSum(nums: number[]): number[] {
+    const prefix: number[] = [];
+
+    prefix[0] = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+
+    return prefix;
+}
